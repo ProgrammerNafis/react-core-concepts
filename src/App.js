5a2b80c475083193f0af3f4e3916d0e1a85d05d2
CyberProgrammmer <sahariyanafis974@gmail.com>
2024-07-12 10:39:41 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+function App() {  
+  const productList = [
+    {name:'alien book',price:540},
+    {name:'funny book',price:530},
+    {name:'face book',price:545},
+    {name:'online book',price:504},
+
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter/>
+    <Userlist/>
+
+     {
+      productList.map(item =>  <Products product={item}></Products>)
+     }
+
+
     </div>
   );
 }
+
+ function Products(props) {  
+  const {name,price} = props.product;
+  const pstly = {
+    color:'white',
+    backgroundColor:'black',
+    padding:'1rem',
+    margin:'10px',
+
+  }
+  return (
+    <div className="App" style={pstly}>
+      <p>{`Name: ${name}`}</p>
+      <p>{`Price: ${price}`}</p>
+      <button>Buy NOw</button>
+
+    </div>
+  );
+}
+
+
+function Counter(){
+  const [count,setCount] = useState(0)
+  return(
+    <div>
+      <h1>count: {count}</h1>
+      <button onClick={()=>{
+        const newCount = count + 1;
+        setCount(newCount)
+      }} style={{padding:'20px',margin:'5px'}}>Increase</button>
+       <button onClick={()=>{
+        const deCount = count - 1;
+        setCount(deCount)
+       }} style={{padding:'20px',margin:'5px'}}>Decrease</button>
+    </div>
+  );
+};
+
+
+function Userlist(){
+  const [user,setUser] = useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users/')
+      .then(response => response.json())
+      .then(data => setUser(data))
+  },[])
+
+  return(
+    <div>
+      <h2>Api Users Length:{user.length}</h2>
+      <li>
+       {
+       user.map(usr=> <li>{usr.name}</li> )
+       }
+      </li>
+    </div>
+  );
+};
 
 export default App;
